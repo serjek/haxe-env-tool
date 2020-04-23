14b288ce5ae8963d2d55b306ec8fd8326c9f1c7f
Sergey Balitskiy <sergey.balitskiy@doclerholding.com>
2020-04-23 17:37:04 +0200

    allow Int env vars (useful for port)

--- src/com/macro/EnvTool.hx
+++ src/com/macro/EnvTool.hx
@@ -14,21 +14,39 @@ class EnvTool
 		for (m in builder) {
 			var PROP = m.name.toUpperCase();
 			var GETTER = 'get_'+PROP;
-			var mvar =  switch m.getVar() {
-				case Success(mvar): mvar.expr;
+			var mvar = switch m.getVar() {
+				case Success(mvar): 
+					{
+						expr: mvar.expr,
+						type: switch (mvar.type) {
+							case TPath(t): t.name;
+							case _: throw ('not valid type');
+						}
+					};
 				case _: throw('not a valiable');
 			};
 
-			var expr = macro Sys.getEnv($v{PROP}) != null 
-				? Sys.getEnv($v{PROP}) 
-				: $e{mvar};
-			
+			var expr = if (mvar.type == "Int") 
+					macro Sys.getEnv($v{PROP}) != null 
+						? $v{Std.parseInt(Sys.getEnv($v{PROP}))}
+						: $e{mvar.expr}
+				else 
+					macro Sys.getEnv($v{PROP}) != null 
+						? Sys.getEnv($v{PROP})
+						: $e{mvar.expr};
+
 			builder.removeMember(m);
 
-			builder.addMembers(macro class {
-				public static var $PROP(get, never):String;
-				static function $GETTER():String return $expr;
-			});
+			if (mvar.type == "Int") 
+				builder.addMembers(macro class {
+					public static var $PROP(get, never):Int;
+					static function $GETTER():Int return $expr;
+				})
+			else 
+				builder.addMembers(macro class {
+					public static var $PROP(get, never):String;
+					static function $GETTER():String return $expr;
+				});
 		}
 
 		return builder.export();
